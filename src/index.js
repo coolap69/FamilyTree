f734d3b5dce0b1111adb54168a1edf3eb6d94a73
coolap69 <coolap69@aol.com>
2018-10-09 15:32:01 -0400

    fixed index page and css

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-// window.onload = function (e) {
-window.onload.href="upsert-famtree.html" = function (e) {
+window.onload = function (e) {
+// window.onload.href="upsert-famtree.html" = function (e) {
     // debugger;
 
     if (typeof window.sessionStorage.loggedIn === "undefined" || window.sessionStorage.loggedIn === null || window.sessionStorage.loggedIn === "") {
@@ -20,55 +20,55 @@ var loadStories = function () {
     const myStoriesNode = document.getElementById("my-stories");
     const otherStoriesNode = document.getElementById("other-stories");
 
-    // Get list of stories
-    let stories = window.localStorage.stories ? JSON.parse(window.localStorage.stories) : [];
+//     // Get list of stories
+//     let stories = window.localStorage.stories ? JSON.parse(window.localStorage.stories) : [];
 
-    // Loop through list of stories
-    for (let index = 0; index < stories.length; index++) {
-        const story = stories[index];
+//     // Loop through list of stories
+//     for (let index = 0; index < stories.length; index++) {
+//         const story = stories[index];
 
-        // Display each story that belongs the logged in user
-        if (story.createdBy == window.sessionStorage.loggedIn) {
-            const myStoryNode = createFamilyNode(story);
+//         // Display each story that belongs the logged in user
+//         if (story.createdBy == window.sessionStorage.loggedIn) {
+//             const myStoryNode = createFamilyNode(story);
 
-            myStoriesNode.appendChild(myStoryNode);
-        } else {  // Display other stories as well
-            const otherStoryNode = createFamilyNode(story.id);
+//             myStoriesNode.appendChild(myStoryNode);
+//         } else {  // Display other stories as well
+//             const otherStoryNode = createFamilyNode(story.id);
 
-            otherStoriesNode.appendChild(otherStoryNode);
-        }
-    }
+//             otherStoriesNode.appendChild(otherStoryNode);
+//         }
+//     }
 }
 
-/**
- * 
- * @param {*} story 
- */
-var createFamilyNode = function (story) {
-    var storyNode = document.createElement("div");
-    var detailsNode = document.createElement("a");
-    var editNode = document.createElement("a");
-    var deleteNode = document.createElement("div");
+// /**
+//  * 
+//  * @param {*} story 
+//  */
+// var createFamilyNode = function (story) {
+//     var storyNode = document.createElement("div");
+//     var detailsNode = document.createElement("a");
+//     var editNode = document.createElement("a");
+//     var deleteNode = document.createElement("span");
 
-    detailsNode.setAttribute("href", "famtree-details.html?id=" + story.id);
-    detailsNode.innerHTML = '<span class="story-title">' + story.title + '</span> |';
+//     detailsNode.setAttribute("href", "famtree-details.html?id=" + story.id);
+//     detailsNode.innerHTML = '<span class="story-title">' + story.title + '</span> |';
 
-    editNode.setAttribute("href", "famtree-details.html?id=" + story.id);
-    editNode.innerHTML = '<span class="edit-button"> \
-        <i>Edit</i> \
-    </span> |';
+//     editNode.setAttribute("href", "famtree-details.html?id=" + story.id);
+//     editNode.innerHTML = '<span class="edit-button"> \
+//         <i>Edit</i> \
+//     </span> |';
 
-    deleteNode.setAttribute("href", "famtree-details.html?id=" + story.id);
-    deleteNode.innerHTML = '<span class="delete-button"> \
-    <em>Delete</em> \
-    </span> |';
+//     deleteNode.setAttribute("href", "famtree-details.html?id=" + story.id);
+//     deleteNode.innerHTML = '<span class="delete-button"> \
+//     <em>Delete</em> \
+//     </span> |';
 
-    storyNode.appendChild(detailsNode);
-    storyNode.appendChild(editNode);
-    storyNode.appendChild(deleteNode);
+//     storyNode.appendChild(detailsNode);
+//     storyNode.appendChild(editNode);
+//     storyNode.appendChild(deleteNode);
 
-    return storyNode;
-}
+//     return storyNode;
+// }
 
 // document.getElementsByClassName("delete-button").addEventListener("click", function (e) {
 //     // debugger;
@@ -76,4 +76,4 @@ var createFamilyNode = function (story) {
 //     e.preventDefault();
 
 //     alert("Clicked delete button.");
-});
+// });
